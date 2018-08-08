96c1a5edfffad20047dbf86eb7472cd0721fb39e
andrecarlos459 <42214715+andrecarlos459@users.noreply.github.com>
2018-08-08 12:53:23 -0300

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,52 @@
+var questions = [{
+  question: "O que fazer numa Universidade? ",
+  choices: ['Aproveite os primeiros períodos para se dedicar exclusivamente aos estudos e atividades acadêmicas no geral, se possível.', 'Ficar adiando as atividades.', 'Estudar para as provas um dia antes da mesma.', 'Faltar a maioria das aulas.'],
+  correctAnswer: 0,
+  answerLocation: '"linkaqui1"'
+}, {
+  question: "Durante a palestra Lidiane afirmou que quando estagiou na Fundaj como analista de suporte, não se sentia realizada profissionalmente, pois, ela queria atuar desenvolvimento. Mesmo trabalhando em um cargo que ela não queria, Lidiane continuou dando o seu melhor. Segundo Lidiane, por que ela fez isso?",
+  choices: ['Ela fez isso, apenas porque necessitava do emprego.','Durante a palestra, Lidiane enfatizou que independente de qual seja o cargo, temos que dar o nosso melhor.','Na verdade, Lidiane não deu o seu melhor e executou suas atividades apenas por obrigação.','N.D.A.'],
+  correctAnswer: 1,
+  answerLocation: '"linkaqui2"'
+}, {
+  question: "Rubson Lima estagiário em desenvolvimento de software da Fundaj, explanou suas experiencias como desenvolvedor e também deu algumas dicas à respeito do aprendizado de linguagens de programação, suas aplicações e sobre tecnologia em geral. Qual das assertivas abaixo não corresponde a características do desenvolvedor?",
+  choices: ['O desenvolvedor tem que ter em mente que tecnologia é meio e não fim.', 'O desenvolvedor deve estar focado nos conceitos e não nas linguagens.', 'O desenvolvedor deve estar motivado a aprender.', 'O desenvolvedor não precisa se atualizar a novas tecnologias.'],
+  correctAnswer: 3,
+  answerLocation: '"linkaqui3"'
+}, {
+  question: "Gustavo Magalhães e Rodrigo Hime ao ministrarem a palestra com o tema: “Da academia para o mundo”, ambos deram algumas dicas à respeito da faculdade e da rotina de trabalho. Sobre essas dicas assinale a alternativa correta:",
+  choices: ['A faculdade ensina tudo o que precisa para entrar no mercado de trabalho.', 'Não participe de atividades na faculdade.', 'Não a necessidade aprender a programar.', 'Seja PHD em priorização.'],
+  correctAnswer: 3,
+  answerLocation: '"linkaqui4"'
+}, {
+  question: "O que é este esteganografia?",
+  choices: ['É um tipo de arte renascentista.', 'É um tipo de escrita usada pelos povos gregos.', 'Esteganografia é o ramo particular da criptologia que consiste em fazer com que uma forma escrita seja camuflada em outra a fim de mascarar o seu verdadeiro sentido.', 'É um ramo da computação que não possui relação nenhuma com a criptografia.'],
+  correctAnswer: 2,
+  answerLocation: '"linkaqui5"'
+}, {
+  question: "O que é criptografia? ",
+  choices: ['Estuda a construção e análise de protocolos que impedem terceiros (ou o público) de lerem mensagens privadas.', 'É um ramo particular da esteganografia.', 'É um tipo de escrita através de códigos usando apenas letras minúsculas.', 'N.D.A.'],
+  correctAnswer: 0,
+  answerLocation: '"linkaqui1"'
+}, {
+  question: "O que é texto claro?",
+  choices: ['É um texto utilizando letras de cores claras.','É um texto escrito em um papel branco ou de cor clara.','Mensagem original, antes de ser “processada” e que é propriedade do emissário.','É um texto que possui coerência e coesão.'],
+  correctAnswer: 2,
+  answerLocation: '"linkaqui2"'
+}, {
+  question: "O que é texto cifrado?",
+  choices: ['É um texto que possui as cifras de uma música.', 'Texto obtido a partir da mensagem original “processando-a” através do algoritmo e utilizando a respectiva chave.', 'É um texto que foi processado e transformado em um código utilizando apenas letras minúsculas.', 'N.D.A.'],
+  correctAnswer: 1,
+  answerLocation: '"linkaqui3"'
+}, {
+  question: "Na criptografia, o que é chave?",
+  choices: ['É uma senha para abrir um determinado programa.', 'É uma ferramenta para permitir o acesso ao computador.', 'Informação necessária para obter a mensagem original a partir do texto cifrado. Só acessível às pessoas autorizadas.', 'N.D.A'],
+  correctAnswer: 2,
+  answerLocation: '"linkaqui4"'
+}, {
+  question: "Na criptografia, o que é algoritmo?",
+  choices: ['Processo através do qual uma mensagem é transformada em um texto cifrado.', 'É um método específico de organizar os algarismos.', 'Um calculo que muda a posição dos algarismos.', 'N.D.A'],
+  correctAnswer: 0,
+  answerLocation: '"linkaqui5"'
+}];
+
